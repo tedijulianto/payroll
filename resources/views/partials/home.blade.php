5fb30d4fd67d6757b74b95cc99b1a98f46616e3e
tedijulianto <tedijulianto92@gmail.com>
2022-05-25 15:46:09 +0700

    Upload to Github

---
+++ resources/views/partials/home.blade.php
@@ -0,0 +1,94 @@
+<div class="bgGradient">
+  <div class="d-flex justify-content-between align-items-center px-3">
+    <div class="d-flex align-items-center">
+      <img src="{{ asset('images/cewe.jpg') }}" alt="User Photo" class="rounded-circle imgUser">
+      <div class="ms-2">
+        <h6 class="fontSpace text-uppercase">Shakira</h6>
+        <h6 class="subtitle fw-light text-white fontSpace">Programmer</h6>
+      </div>
+    </div>
+    <button class="text-white d-flex gap-2 bg-transparent border-0"><img src="images/bx_bxs-bell-ring.png"
+        alt="bell ring">0</button>
+  </div>
+
+  <div class="p-3">
+    <div class="bg-white rounded-3 p-2">
+      <div class="d-flex justify-content-between align-items-centers">
+        <p class="fontSpace fw-light">Profil Perusahaan</p>
+        <p class="fontSpace fw-bold">TADS</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="d-flex justify-content-between px-3 gap-3">
+    <div style="background-image: url('images/bg-card.png'); background-repeat: no-repeat; background-size: cover;"
+      class="rounded-3 px-14 py-10 border border-info cardPadding position-relative">
+      <p class="text-white fw-bold cardText1 fontSpace">Kasbon</p>
+      <div class="rectangle"></div>
+      <p class="text-white cardText2 fontSpace">Rp. 4.000.000</p>
+    </div>
+
+    <div style="background-image: url('images/bg-card.png'); background-repeat: no-repeat; background-size: cover;"
+      class="rounded-3 border border-info cardPadding position-relative">
+      <p class="text-white fw-bold cardText1 fontSpace">Cuti</p>
+      <div class="rectangle"></div>
+      <p class="text-white cardText2 fontSpace">12 Hari Tersisa</p>
+    </div>
+  </div>
+</div>
+
+<div class="paddingContent bg-white">
+  <h6 class="text-primary fontSpace">Pengumuman</h6>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+  <div class="d-flex justify-content-between align-items-centers py-3 border-bottom">
+    <p class="subtitle">Divisi Tele dan CRM</p>
+    <p class="subtitle">02/02/2022</p>
+  </div>
+</div>
